27bb4a88d0a50230d00f11f0b63640a377156a20
Yourstruggle11 <sensouvik51@gmail.com>
2022-09-24 23:02:56 +0530

    `feat`: Added `write` func to write `tempalte code` to a `file`

---
+++ utils/write.js
@@ -0,0 +1,15 @@
+import fs from "fs"
+//**
+//  * echo str > file.
+//  *
+//  * @param {String} file
+//  * @param {String} str
+//**
+
+import { MODE_0666 } from "../config/constants.js"
+
+export const write =  (file, str, mode) => {
+    fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
+    console.log('   \x1b[36mcreate\x1b[0m : ' + file)
+  }
+  
