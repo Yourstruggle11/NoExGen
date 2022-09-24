510045538790b26d26261bd8e901b35db9c34c6b
Yourstruggle11 <sensouvik51@gmail.com>
2022-09-24 23:20:27 +0530

    `feat`: Added `main`, `createApp` func to `NoExGen-cli`

--- utils/usage.js
+++ utils/usage.js
@@ -12,7 +12,7 @@ export const  usage =  () => {
     console.log('    --view=jade          create NoEx template With ejs engine')
     console.log('    --view=pug           create NoEx template With pug engine')
     console.log('    --view=hbs           create NoEx template With handlebars engine')
-    console.log('    --view=hjs         create NoEx template With hogan.js engine')
+    console.log('    --view=hjs           create NoEx template With hogan.js engine')
     console.log('    -v, --view <engine>  create NoEx template with view <engine> support (dust|ejs|hbs|hjs|jade|pug|twig|vash) (defaults to jade)')
     console.log('        --no-view        use static html instead of view engine')
     console.log('    -c, --css <engine>   add stylesheet <engine> support (less|stylus|compass|sass) (defaults to plain css)')
