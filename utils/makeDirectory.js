f095c6340ebb6308b3dd94ae49d6d33b238d99ed
Yourstruggle11 <sensouvik51@gmail.com>
2022-09-24 23:00:20 +0530

    `feat`: Added a `makeDir` func to create a dir relatetive to base dir

---
+++ utils/makeDirectory.js
@@ -0,0 +1,16 @@
+import mkdirp from 'mkdirp';
+import path from 'path';
+import { MODE_0755 } from '../config/constants.js';
+
+//**
+//  * Make the given dir relative to base.
+//  *
+//  * @param {string} base
+//  * @param {string} dir
+//  */
+
+ export const mkdir = (base, dir) => {
+    var loc = path.join(base, dir);
+    console.log('   \x1b[36mcreate\x1b[0m : ' + loc + path.sep)
+    mkdirp.sync(loc, MODE_0755)
+  }
